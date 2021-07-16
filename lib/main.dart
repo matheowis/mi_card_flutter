e0fdb4ba0af0426714d5ed0fd28e7300f4ce2c3c
Wiśniowski Mateusz <mateusz.wisniowski@wisniowski.pl>
2021-07-16 15:05:00 +0200

    Added divider

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,13 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                 ),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color:Colors.teal.shade100
+                ),
+              ),
               Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: ListTile(
